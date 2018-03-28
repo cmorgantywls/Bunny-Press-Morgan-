20560558509ca4753314b6c678c856f60f2d129d
cmorgantywls <cmorgan@tywlsbronx.org>
2018-03-28 13:09:02 -0400

    Upperline - Bunny Press
    Upperline Bunny Press Project

--- Bunny Press (Morgan)/ViewController.swift
+++ Bunny Press (Morgan)/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var theStatus: UILabel!
+    @IBOutlet weak var bunImageView: UIImageView!
+    @IBOutlet weak var theTitle: UILabel!
+    //sorry I've been watching a lot of Vines?
+    var counter = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //will change to default settings.
+        theStatus.text = "Need pets plz."
     }
 
+    //not using this one
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func onBunPress(_ sender: Any) {
+        counter += 1;
+        if counter == 1 {
+            print("You have pt the bun \(counter) time.")
+        }
+        else {
+        print("You have pet the bun \(counter) times.")
+        }
+        if counter >= 10 {
+            bunImageView.image = #imageLiteral(resourceName: "bunnyangry")
+            theStatus.text = "TOO MANY PETS."
+        }
+    }
+    
 }
 
